de0e731455080080428f828bafb1eebe6feaa442
RiverRobins <rivergrahamrobins@gmail.com>
2019-12-17 15:04:40 -0600

    i already do not like sql

---
+++ albums_migration.sql
@@ -0,0 +1,10 @@
+USE codeup_test_db;
+CREATE TABLE IF NOT EXISTS albums(
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+artist VARCHAR(50) NOT NULL,
+name VARCHAR(50) NOT NULL,
+release_date INT,
+sales DOUBLE,
+genre VARCHAR(50),
+PRIMARY KEY(id)
+);
