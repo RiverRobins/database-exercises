a1fb1a2861ade0c0148d238beaf8c56962ff92b9
RiverRobins <rivergrahamrobins@gmail.com>
2019-12-18 11:48:13 -0600

    added select excersises

---
+++ select_exercises.sql
@@ -0,0 +1,10 @@
+USE codeup_test_db;
+SELECT * FROM albums;
+SELECT * FROM albums WHERE artist = 'Pink Floyd';
+SELECT * FROM albums WHERE release_date = 1967;
+SELECT * FROM albums WHERE genre LIKE 'Grunge'
+    OR genre LIKE 'Alternative Rock';
+SELECT * FROM albums WHERE release_date >= 1990
+    AND release_date < 2000;
+SELECT * FROM albums WHERE sales < 20;
+SELECT * FROM albums WHERE genre = 'Rock';
