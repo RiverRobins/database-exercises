0d74dba151110091f986bdef9f5f22a58bc6abf5
RiverRobins <rivergrahamrobins@gmail.com>
2019-12-18 11:25:54 -0600

    added albums_seeder.sql

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,7 +2,7 @@ USE codeup_test_db;
 CREATE TABLE IF NOT EXISTS albums(
 id INT UNSIGNED NOT NULL AUTO_INCREMENT,
 artist VARCHAR(50) NOT NULL,
-name VARCHAR(50) NOT NULL,
+name VARCHAR(150) NOT NULL,
 release_date INT,
 sales DOUBLE,
 genre VARCHAR(50),
